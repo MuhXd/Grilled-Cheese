bddbb8d64e1ca01989be9275a4d0feaee4ccc4d5
MuhXd <momo2010sofia@gmail.com>
2024-02-25 07:57:53 +0000

    SDJ

--- src/CheeseMake.cpp
+++ src/CheeseMake.cpp
@@ -7,12 +7,12 @@
 #include <iostream>
 void Script(int scriptnum) {
     if (scriptnum == 1) {
-        int pause = 0
+        int pause = 0;
         auto timer = CCLabelBMFont::create("", "chatFont.fnt");
           while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(0.5));
-        pause+=0.5
-        pause->setString(pause);
+        pause+=0.5;
+        pause->setString(std::string(pause));
         log::info("Paused for added 0.5");
         if (pause >= 3) {
 log::info("Paused for 3s");
